c221663e902dcdbb22addbbb0cd023b2e47db5a9
YashasHewage <yashrashh05@gmail.com>
2023-11-08 11:09:18 +0530

    change

--- index.js
+++ index.js
@@ -4,18 +4,15 @@ document.addEventListener("DOMContentLoaded", function () {
     // statement should be the "statement" div
     const statement = document.getElementById("statement");
     // optionButtons should be all the elements within the "options" div
-   
-
+    const answer = document.getElementById("options").children;
     // explanation should be the "explanation" div
     const explanation = document.getElementById("explanation");
-
-
     // TODO 2: Declare & assign a variable called fact
     // Its value should be an object with a statement, true/false answer, and explanation 
     const fact = {
       statement: "My name is yashas ",
       answer: true ,
-      explanation: "Your name is yashas"
+      explanation: "The creater of this project is Yashas"
     }
   
     // TODO 3: Set the text of the statement element to the fact's statement
@@ -25,17 +22,13 @@ document.addEventListener("DOMContentLoaded", function () {
     // TODO 4: Declare disable & enable functions to set or remove the "disabled" attribute from a given button element
     
     // disable(button) should set the button element's attribute "disabled" to the value ""
-    const disable = (button) => button.setAttribute("disabled","")
+
     // enable(button) should remove the attribute "disabled" from the button element
-    const enable = (button) => button.removeAttribute("disabled");
+ 
 
     // TODO 5: Declare an isCorrect function that compares a guess to the right answer
     // isCorrect(guess) should return true if the guess matches the fact's answer
-    function isCorrect(guess) {
-      if (fact.answer = true){
-        return true;
-      }
-    }
+  
 
 
     // TODO 6A: Use a for loop to add a click event listener to each of the optionButtons
