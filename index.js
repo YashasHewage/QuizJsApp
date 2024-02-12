bdc10b7a1bd9f555831523469f6a13fa27dc9dde
YashasHewage <yashrashh05@gmail.com>
2024-02-12 13:24:24 +0530

    fadsfasd

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function disable (button){
 
 
 function enable(button){
-      button.removeAttribute("disabled","");lfdddfasdfas
+      button.removeAttribute("disabled","");lfdddfadfasdads
     }
    
 
